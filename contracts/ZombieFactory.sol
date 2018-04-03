831ac13989216447cb32d4a7bc16b2998d8f3f53
Luke Schoen <ltfschoen@gmail.com>
2018-04-03 10:44:37 +1000

    feat: Assign new zombie to owner (message sender) and increase their zombie count

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -21,6 +21,9 @@ contract ZombieFactory {
 
     function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // Update `zombieToOwner` mapping to store `msg.sender` under `id`
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
     } 
 
